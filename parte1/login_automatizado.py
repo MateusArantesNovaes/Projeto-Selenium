faa25637f216aa5a0c403e902e7639455fa89bf7
Mateus <mat.a.novaes@gmail.com>
2025-05-22 12:59:14 -0300

    Preencha usuário

--- parte1/login_automatizado.py
+++ parte1/login_automatizado.py
@@ -10,6 +10,10 @@ navegador.get('https://www.saucedemo.com/')
 #Abrir o site em tela cheia
 navegador.maximize_window()
 
+#preencher o usuário
+campo_nome = navegador.find_element("id", "user-name")
+campo_nome.send_keys("standard_user")
+
 #Definindo um tempo para o navegador ficar aberto
 time.sleep(10)
 
