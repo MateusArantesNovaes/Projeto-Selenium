70af6baf72ae64c04538e6e0c835499f248a3f95
Mateus <mat.a.novaes@gmail.com>
2025-05-22 16:29:25 -0300

    definido func

--- parte1/login_automatizado.py
+++ parte1/login_automatizado.py
@@ -1,26 +1,27 @@
 from selenium import webdriver
 import time
 
-#Abrir o navegador 
-navegador = webdriver.Firefox()
+def login():
+    #Abrir o navegador 
+    navegador = webdriver.Firefox()
 
-#Acessar o site
-navegador.get('https://www.saucedemo.com/')
+    #Acessar o site
+    navegador.get('https://www.saucedemo.com/')
 
-#Abrir o site em tela cheia
-navegador.maximize_window()
+    #Abrir o site em tela cheia
+    navegador.maximize_window()
 
-#preencher o usuário
-campo_nome = navegador.find_element("id", "user-name")
-campo_nome.send_keys("standard_user")
+    #preencher o usuário
+    campo_nome = navegador.find_element("id", "user-name")
+    campo_nome.send_keys("standard_user")
 
-#preencher a senha
-campo_senha = navegador.find_element("id", "password")
-campo_senha.send_keys("secret_sauce")
+    #preencher a senha
+    campo_senha = navegador.find_element("id", "password")
+    campo_senha.send_keys("secret_sauce")
 
-#clicando no botão de login
-botao_login = navegador.find_element("id", "login-button")
-botao_login.click()
+    #clicando no botão de login
+    botao_login = navegador.find_element("id", "login-button")
+    botao_login.click()
 
-#Definindo um tempo para o navegador ficar aberto
-time.sleep(10)
+    #Definindo um tempo para o navegador ficar aberto
+    time.sleep(10)
