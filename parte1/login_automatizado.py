a32dd7e61df909a070352d17d6eb7b82a0b93e64
Mateus <mat.a.novaes@gmail.com>
2025-05-22 12:41:19 -0300

    Acessa o site

--- parte1/login_automatizado.py
+++ parte1/login_automatizado.py
@@ -3,6 +3,12 @@ import time
 
 #Abrir o navegador 
 navegador = webdriver.Chrome()
+
+#Acessar o site
+navegador.get('https://www.saucedemo.com/')
+
 #Definindo um tempo para o navegador ficar aberto
 time.sleep(10)
+
+
 
