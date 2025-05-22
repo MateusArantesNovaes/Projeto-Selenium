204e2ee28d374bf4b5807a3654f76e4c6c932bdf
Mateus <mat.a.novaes@gmail.com>
2025-05-22 12:52:04 -0300

    Abra o navegador em tela cheia

--- parte1/login_automatizado.py
+++ parte1/login_automatizado.py
@@ -7,6 +7,9 @@ navegador = webdriver.Chrome()
 #Acessar o site
 navegador.get('https://www.saucedemo.com/')
 
+#Abrir o site em tela cheia
+navegador.maximize_window()
+
 #Definindo um tempo para o navegador ficar aberto
 time.sleep(10)
 
